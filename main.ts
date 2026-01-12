bdc083e6799a27709c5967a1cb28d9435e9912c0
marohennawi-del <maro.hennawi@gmail.com>
2026-01-12 17:11:16 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,18 @@
+let random_number = 0
+input.onButtonPressed(Button.A, function () {
+    let random_nuber = 0
+    random_number = 6
+    if (random_number == 1) {
+        basic.showString("go to my bestfriend's house for a whole day ")
+    } else if (random_nuber == 2) {
+        basic.showString("call my bestieee!!!")
+    } else if (random_nuber == 3) {
+        basic.showString("help my mom (not rlly)")
+    } else if (random_nuber == 4) {
+        basic.showString("play with my cousin")
+    } else if (random_nuber == 5) {
+        basic.showString("fight my cousin")
+    } else if ((0 as any) == (6 as any)) {
+        basic.showString("play rblx")
+    }
+})
